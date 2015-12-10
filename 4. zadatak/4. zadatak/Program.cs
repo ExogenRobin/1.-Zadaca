0ac71f0e1cb3d9cd18a7336aebf1116eabe97c72
ExogenRobin <robin1008994@gmail.com>
2015-12-10 17:31:26 +0100

    Dodan 4. zadatak

---
+++ 4. zadatak/4. zadatak/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.zadatak {
+    class Program {
+        static void Main(string[] args) {
+            IGenericList<string> stringList = new GenericList<string>();
+            stringList.Add("Hello");
+            stringList.Add("World");
+            stringList.Add("!");
+            foreach (string value in stringList) {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
