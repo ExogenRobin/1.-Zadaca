680c72d0fc8e0bac2b4e767eb184aa3732b1266d
ExogenRobin <robin1008994@gmail.com>
2015-12-10 03:06:56 +0100

    1. zadatak dodan

---
+++ 1. zadatak/1. zadatak/IntegerList.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.zadatak {
+    public class IntegerList : IIntegerList {
+        private int[] _internalStorage;
+        private int[] _temp;
+        private int _sizeOfArray;
+        private int _count = 0;
+
+        public IntegerList() {
+            _internalStorage = new int[4];
+            _sizeOfArray = 4;
+        }
+
+        public IntegerList(int initialSize) {
+            _internalStorage = new int[initialSize];
+            _sizeOfArray = initialSize;
+        }
+
+        public int Count { get { return _count; } }
+
+        public void Add(int item) {
+            if (_count == _sizeOfArray) {
+                _temp = new int[_sizeOfArray];
+                _internalStorage.CopyTo(_temp, 0);
+
+                _sizeOfArray = _internalStorage.Length * 2;
+                _internalStorage = new int[_sizeOfArray];
+
+                _temp.CopyTo(_internalStorage, 0);
+            }
+            try {
+                _internalStorage[_count] = item;
+                _count++;
+            }
+            catch (IndexOutOfRangeException) {
+
+            }
+
+            
+        }
+
+        public void Clear() {
+            _internalStorage = new int[_sizeOfArray];
+            _count = 0;
+        }
+
+        public bool Contains(int item) {
+            for (int i = 0; i < _sizeOfArray - 1; i++) {
+                if (_internalStorage[i].Equals(item)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int GetElement(int index) {
+            if (index <= _sizeOfArray) {
+                return _internalStorage[index];
+            }
+            else {
+                throw new System.IndexOutOfRangeException();
+            }
+
+        }
+
+        public int IndexOf(int item) {
+            for (int i = 0; i < _sizeOfArray; i++) {
+                if (_internalStorage[i].Equals(item)) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public bool Remove(int item) {
+            if (IndexOf(item) < 0) {
+                return false;
+            }
+            return RemoveAt(IndexOf(item));
+        }
+
+        public bool RemoveAt(int index) {
+            if (index >= _count) {
+                return false;
+            }
+            _internalStorage[index] = 0;
+
+            for (int i = index; i < _sizeOfArray - 1; i++) {
+                _internalStorage[i] = _internalStorage[i + 1];
+            }
+            _internalStorage[_sizeOfArray - 1] = 0;
+            _count--;
+            return true;
+        }
+
+        public void Write() {
+            Console.Write("[");
+            for (int i = 0; i < _sizeOfArray; i++) {
+                Console.Write(_internalStorage[i] + ",");
+            }
+            Console.Write("]\n");
+        }
+    }
+}
---
+++ 1. zadatak/1. zadatak/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.zadatak {
+    class Program {
+        public static void Main(String[] args) {
+            IntegerList listOfIntegers = new IntegerList();
+
+            ListExample(listOfIntegers);
+
+        }
+
+        public static void ListExample(IntegerList listOfIntegers) {
+            listOfIntegers.Add(1);
+            listOfIntegers.Add(2);
+            listOfIntegers.Add(3);
+            listOfIntegers.Add(4);
+            listOfIntegers.Add(5);
+            // lista je [1,2,3,4,5]
+            listOfIntegers.Write();
+            // Mičemo prvi element liste.
+            listOfIntegers.RemoveAt(0);
+            // Lista je [2,3,4,5]
+            listOfIntegers.Write();
+            // Mičemo element liste čija je vrijednost "5".
+            listOfIntegers.Remove(5);
+            // Lista je [2,3,4]
+            listOfIntegers.Write();
+            Console.WriteLine(listOfIntegers.Count);
+            // 3
+            Console.WriteLine(listOfIntegers.Remove(100));
+            // false, nemamo element u vrijednosti 100
+            Console.WriteLine(listOfIntegers.RemoveAt(5));
+            // false, nemamo ništa na poziciji 5
+            // Brišemo sav sadržaj kolekcije
+            listOfIntegers.Clear();
+            Console.WriteLine(listOfIntegers.Count);
+            // 0
+        }
+    }
+}
