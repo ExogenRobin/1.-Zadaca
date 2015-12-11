ce3af6642fbc983f86acf7f879adb7b8577c91c3
ExogenRobin <robin1008994@gmail.com>
2015-12-11 18:36:29 +0100

    Izmjenjena PongGame

--- PongGame/PongGame/CollisionDetector.cs
+++ PongGame/PongGame/CollisionDetector.cs
@@ -12,9 +12,20 @@ namespace PongGame {
         /// <param name="s1">First sprite</param>
         /// <param name="s2">Second sprite</param>
         /// <returns>Returns true if overlapping</returns>
-        public static bool Overlaps(Ball s1, Paddle s2) {
-            return false;
+        public static bool Overlaps(Ball s1, Sprite s2) {
+            float testX = s1.Position.X;
+            float testY = s1.Position.Y;
+
+            if (testX < s2.Position.X)
+                testX = s2.Position.X;
+            if (testX >= (s2.Position.X + s2.Size.Width / 1.5f))
+                testX = (s2.Position.X + s2.Size.Width / 1.5f);
+            if (testY < s2.Position.Y)
+                testY = s2.Position.Y;
+            if (testY >= (s2.Position.Y - s2.Size.Height / 2))
+                testY = (s2.Position.Y - s2.Size.Height / 2);
+
+            return ((s1.Position.X - testX) * (s1.Position.X - testX) + (s1.Position.Y - testY) * (s1.Position.Y - testY)) <= (s1.Size.Width) * (s1.Size.Height);
         }
     }
 }
-
